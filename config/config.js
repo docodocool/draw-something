e9396ac04293b4d3dc00e0d703e2447b11e5e5d4
liushuang <liushuang@jilinxiangyun.com>
2021-02-20 17:23:18 +0800

    分离配置项

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const DB_URL = 'mongodb://manager:main_db_manager@89.208.248.23:27017/main';
+const WOW_TOKEN_USERNAME = '900143fcccbd4eceab3c91fc70a71807';
+const WOW_TOKEN_PWD = 'mfAPIY5e3n7UhFUnvKCoi1h3tjmbHHd9';
+
+module.exports = { DB_URL, WOW_TOKEN_USERNAME, WOW_TOKEN_PWD };
