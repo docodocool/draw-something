f16cebd8997969f47367f2d765ee285776e35c84
victor <1045606768@qq.com>
2021-02-25 20:24:10 +0800

    update server ip

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const DB_URL = 'mongodb://manager:main_db_manager@89.208.248.23:27017/main';
+const DB_URL = 'mongodb://manager:main_db_manager@127.0.0.1:27017/main';
 const WOW_TOKEN_USERNAME = '900143fcccbd4eceab3c91fc70a71807';
 const WOW_TOKEN_PWD = 'mfAPIY5e3n7UhFUnvKCoi1h3tjmbHHd9';
 
